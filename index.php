82fad4cd7e9c4bc12ea32e85a4661a05aadde8df
Himanshu2708 <63806659+Himanshu2708@users.noreply.github.com>
2021-06-05 17:53:42 +0530

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
 </head>
-<body onload="fetch()">
+<body>
 
 
 
